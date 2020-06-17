6aac00cd731b4dcc02d0e972ad491ae4811b38da
karan nakra <karannakra1@gmail.com>
2020-06-17 16:46:39 +0530

    this is not currently on command line will put it in UI in nxt update

---
+++ dictionary/dictionary.py
@@ -0,0 +1,15 @@
+import json
+
+parsed_string = json.load(open('./database/data.json'))
+
+
+def translate(word):
+    if word in parsed_string:
+        return parsed_string[word]
+    else:
+        return "The word doesn't exist.Please double check it"
+
+while(True):
+    word = input('Enter Word:')
+    word=word.lower()
+    print(translate(word))
